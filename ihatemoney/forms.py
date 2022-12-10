abe953906348b07d73142fe2f863b5472e5e40a0
Zhongqi Ma <59981957+mzhongqi@users.noreply.github.com>
2022-12-10 17:26:59 -0500

    Update forms.py

--- ihatemoney/forms.py
+++ ihatemoney/forms.py
@@ -414,7 +414,8 @@ class MemberForm(FlaskForm):
             and Person.query.filter(
                 Person.name == field.data,
                 Person.project == self.project,
-                Person.activated,
+                Person.activated == True,
+                Person.activated == False,
             ).all()
         ):  # NOQA
             raise ValidationError(_("This project already have this participant"))
